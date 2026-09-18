93d541a184006ac342b21001ad86df071b73a166
Loke Yu <yyu17@smith.edu>
2026-09-19 02:37:38 +0800

    Add name and references

--- hw01.py
+++ hw01.py
@@ -1,7 +1,7 @@
 # ------------------------------------------------------
-#        Name: (put your name here)
-#       Peers: (add any collaborators)
-#  References: (anything you checked to solve this)
+#        Name: Loke Yu
+#       Peers: None
+#  References: None
 # ------------------------------------------------------
 
 
